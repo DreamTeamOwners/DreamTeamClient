8e6f29006b189e9ef797dba9264460e6c51a5a15
mataraimov <minuradil@gmail.com>
2023-02-11 16:04:37 +0600

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { Routing } from "pages";
+
+// import { withProviders } from "./providers";
+
+// I don't like idea of re-exporting scss that's weird IMHO that's why I'd go to styles folder
+// import './styles/index.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello weird</h1>
+    </div>
+  );
+};
+export default App;
+// export default withProviders(App);
