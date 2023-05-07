d52ab5851b9e5dc1b7b1ffbbaae868bbfcb045ea
Nuratann <kadyrov.atan@gmail.com>
2023-05-07 21:44:56 +0600

    login form

--- dream-team-front/src/components/screens/auth/login/Login.jsx
+++ dream-team-front/src/components/screens/auth/login/Login.jsx
@@ -1,59 +1,81 @@
-import styles from "./Login.module.css"
-import {Link} from 'react-router-dom'
-import { useState } from "react"
-import {
-    FormControl,
-    FormLabel,
-    Input,
-    FormHelperText,
-    FormErrorMessage,
-    Text,
 
-} from "@chakra-ui/react"
+import React from 'react';
+import { Box, FormControl, FormErrorMessage, FormLabel, Input, Button, Image, HStack } from '@chakra-ui/react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FaFacebook, FaGoogle, FaTwitter } from 'react-icons/fa';
 
-const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+// Валидационная схема с использованием Yup
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Некорректный адрес электронной почты').required('Поле "Email" обязательно для заполнения'),
+  password: Yup.string().required('Поле "Пароль" обязательно для заполнения'),
+});
 
-    const handleEmailChange = (e) => setEmail(e.target.value)
-    const handlePasswordChange = (e) => setPassword(e.target.value)
+const initialValues = {
+  email: '',
+  password: '',
+};
 
-    const isError = (email==='' && password ==='')
+const LoginForm = () => {
+  const handleSubmit = (values, { setSubmitting }) => {
+    // Выполнение действий при отправке формы
+    console.log(values);
+    setSubmitting(false);
+  };
 
     return (
-        <div className={styles.login}>
-            <img src="/login_icon.svg" alt="человек с ноутбуком на черном фоне" />
-            <FormControl isInvalid={isError}>
-                <Text fontSize={24}>Log in</Text>
-                <FormLabel>Email</FormLabel>
-                <Input type='email' value={email} onChange={handleEmailChange} />
-                {!(email==='') ? (
-                    <FormHelperText>
-                        Enter the email.
-                    </FormHelperText>
-                ) : (
-                    <FormErrorMessage>Email is required.</FormErrorMessage>
-                )}
-                <FormLabel>Password</FormLabel>
-                <Input type='password' value={password} onChange={handlePasswordChange} />
-                {!(password==='') ? (
-                    <FormHelperText>
-                        Enter the password.
-                    </FormHelperText>
-                ) : (
-                    <FormErrorMessage>Password is required.</FormErrorMessage>
-                )}
-            </FormControl>
-            <div className={styles.form}>
-                <p>Log in</p>
-                <Link><img src="/google" alt="" /></Link>
-                <Link><img src="/facebook" alt="" /></Link>
-                <Link><img src="/github" alt="" /></Link>
-                <p>Email</p>
-                <p>Password</p>
-            </div>
-        </div>
-    )
-}
+        <Box maxW="md" mx="auto" mt={8} p={4} display="flex" alignItems="center">
+            <Box flex={1} mr={4}>
+                <Image src="/login_icon.svg" alt="Image" w="200px" h="200px" objectFit="cover" />
+            </Box>
+            <Box flex={1}>
+                <HStack spacing={4} mb={4}>
+                    <Button colorScheme="blue" leftIcon={<FaFacebook />}>
+                        Facebook
+                    </Button>
+                    <Button colorScheme="red" leftIcon={<FaGoogle />}>
+                        Google
+                    </Button>
+                    <Button colorScheme="teal" leftIcon={<FaTwitter />}>
+                        Twitter
+                    </Button>
+                </HStack>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Field name="email">
+                                {({ field, form }) => (
+                                    <FormControl isInvalid={form.errors.email && form.touched.email}>
+                                        <FormLabel htmlFor="email">Email</FormLabel>
+                                        <Input {...field} id="email" placeholder="Введите ваш email" />
+                                        <ErrorMessage name="email" component={FormErrorMessage} />
+                                    </FormControl>
+                                )}
+                            </Field>
 
-export default Login
+                            <Field name="password">
+                                {({ field, form }) => (
+                                    <FormControl isInvalid={form.errors.password && form.touched.password}>
+                                        <FormLabel htmlFor="password">Пароль</FormLabel>
+                                        <Input {...field} type="password" id="password" placeholder="Введите ваш пароль" />
+                                        <ErrorMessage name="password" component={FormErrorMessage} />
+                                    </FormControl>
+                                )}
+                            </Field>
+
+                            <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+                                Войти
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
+            </Box>
+        </Box>
+    );
+};
+
+export default LoginForm;
